2468d1b4ce9fc416ba4c4fe3b02e26767752082f
consuelita <adelita@operatorfoundation.org>
2021-06-09 13:36:49 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,30 +3,45 @@
 
 import PackageDescription
 
+#if os(iOS) || os(macOS) || os(watchOS) || os(tvOS)
 let package = Package(
     name: "KeychainLinux",
-    platforms: [.macOS(.v10_15)],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "KeychainLinux",
-            targets: ["KeychainLinux"]),
-    ],
+    platforms: [.macOS(.v11)],
+    products: [.library(name: "KeychainLinux", targets: ["KeychainLinux"]),],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.2"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.6"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "KeychainLinux",
             dependencies: [
-                .product(name: "Crypto", package: "swift-crypto", condition: .when(platforms: [.linux]))
-            ]),
+                .product(name: "Crypto", package: "swift-crypto", condition: .when(platforms: [.linux]))]),
         .testTarget(
             name: "KeychainLinuxTests",
             dependencies: ["KeychainLinux"]),
     ],
     swiftLanguageVersions: [.v5]
 )
+#elseif os(Linux)
+let package = Package(
+    name: "KeychainLinux",
+    products: [
+        .library(
+            name: "KeychainLinux",
+            targets: ["KeychainLinux"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.6"),
+    ],
+    targets: [
+        .target(
+            name: "KeychainLinux",
+            dependencies: [
+                .product(name: "Crypto", package: "swift-crypto")]),
+        .testTarget(
+            name: "KeychainLinuxTests",
+            dependencies: ["KeychainLinux"]),
+    ],
+    swiftLanguageVersions: [.v5]
+)
+#endif

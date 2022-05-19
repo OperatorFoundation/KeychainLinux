289bdb0c7f16e9185c040370bb1b36d1dcbc1423
Unseelie <emunseelie@gmail.com>
2022-05-19 13:36:59 -0500

    updated package platform to have iOS

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "KeychainLinux",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS(.v10_15),
+                .iOS(.v15)],
     products: [
         .library(
             name: "KeychainLinux",

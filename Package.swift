2ad9861d3123c80e95392ec2e4847428ebff783e
bragelbytes <joebragel@gmail.com>
2023-09-29 16:09:31 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["KeychainLinux"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", from: "2.5.0"),
         .package(url: "https://github.com/OperatorFoundation/Gardener", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/KeychainTypes", branch: "main"),
     ],

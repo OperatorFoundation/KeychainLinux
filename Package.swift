5948525d31c5aa481f1d0737c19eed93065adda3
consuelita <adelita@operatorfoundation.org>
2024-02-18 14:02:12 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["KeychainLinux"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto", from: "2.5.0"),
+        .package(url: "https://github.com/apple/swift-crypto", from: "3.2.0"),
         .package(url: "https://github.com/OperatorFoundation/Gardener", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/KeychainTypes", branch: "main"),
     ],
